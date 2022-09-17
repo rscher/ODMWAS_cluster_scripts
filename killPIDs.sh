6f61c1e97bfb8ea3e1c1cee6d2d3062d4dfa4123
Russell Scher <42454585+rscher@users.noreply.github.com>
2022-09-16 19:20:35 -0700

    utility to kill all pids of a keyword
    useful; to kill all java procs in a cluster
    $ killPIDs.sh  java

---
+++ killPIDs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# 
+# killPIDs.sh $1
+#------------------------------
+delay="20s"
+
+if [ -z $1 ] ; then
+ echo "usage: killPIDs.sh <arg1>"
+ echo " kills all pids containing arg1 after a $delay delay."
+ echo " example: $ killPIDs.sh java"
+ echo "   kills all running java processes"
+ exit
+fi
+
+pids=$( ps -ef | grep $1  | gawk '{ print $2 }'  )
+if [[ $pids ]] ; then 
+ echo "killing pids all pids containing $1 in $delay, CNTL-C now to abort."
+ echo ""
+ sleep $delay 
+ for pid in $pids ;  do
+   if [[ $(ps --no-headers -p $pid) ]] ; then 
+     kill -9 $pid
+   fi 
+ done
+fi
